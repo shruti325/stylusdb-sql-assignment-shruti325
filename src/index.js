dc686ba94ce92a04877ba326d9d42ad29a21aee7
shruti325 <sharmashruti71930@gmail.com>
2024-04-23 23:05:35 +0530

    20 steps

--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
-// const parseQuery = require("./queryParser");
-// const readCSV = require("./csvReader");
+const { readCSV, writeCSV } = require("./csvReader");
+const {
+  parseSelectQuery,
+  parseInsertQuery,
+  parseDeleteQuery,
+} = require("./queryParser");
+const {
+  executeSELECTQuery,
+  executeINSERTQuery,
+  executeDELETEQuery,
+} = require("./queryExecutor");
 
-// async function executeSELECTQuery(query) {
-//   const { fields, table } = parseQuery(query);
-//   const data = await readCSV(`${table}.csv`);
-
-//   return data.map((row) => {
-//     const filteredRow = {};
-//     fields.forEach((field) => {
-//       filteredRow[field] = row[field];
-//     });
-//     return filteredRow;
-//   });
-// }
-
-// module.exports = executeSELECTQuery;
-
-const parseQuery = require("./queryParser");
-const readCSV = require("./csvReader");
-
-async function executeSELECTQuery(query) {
-  const { fields, table, whereClause } = parseQuery(query);
-  const data = await readCSV(`${table}.csv`);
-
-  // Filtering based on WHERE clause
-  const filteredData = whereClause
-    ? data.filter((row) => {
-        const [field, value] = whereClause.split("=").map((s) => s.trim());
-        return row[field] === value;
-      })
-    : data;
-
-  // Selecting the specified fields
-  return filteredData.map((row) => {
-    const selectedRow = {};
-    fields.forEach((field) => {
-      selectedRow[field] = row[field];
-    });
-    return selectedRow;
-  });
-}
-
-module.exports = executeSELECTQuery;
+module.exports = {
+  readCSV,
+  writeCSV,
+  executeSELECTQuery,
+  executeINSERTQuery,
+  executeDELETEQuery,
+  parseSelectQuery,
+  parseInsertQuery,
+  parseDeleteQuery,
+};
